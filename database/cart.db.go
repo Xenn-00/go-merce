19a67f0558e698bf356c94ca76f227efbb612843
Xenn-00 <node.xenon@gmail.com>
2023-04-08 13:04:55 +0700

    update

--- database/cart.db.go
+++ database/cart.db.go
@@ -1,1 +1,21 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("sorry, we can't find the products")
+	ErrCantDecodeProducts = errors.New("sorry, we can't find the products")
+	ErrUserIdIsNotValid   = errors.New("sorry, this user is not valid")
+	ErrCantUpdateUser     = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("can't remove this item from the cart")
+	ErrCantGetItems       = errors.New("unable to get the item from the cart")
+	ErrCantBuyCartItem    = errors.New("can't update the purchase")
+)
+
+func AddProductToCart()
+
+func RemoveCartItem()
+
+func BuyItemFromCart()
+
+func InstantBuyer()
--- database/database.setup.go
+++ database/database.setup.go
@@ -1,1 +1,14 @@
 package database
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSetup() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(""))
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection
