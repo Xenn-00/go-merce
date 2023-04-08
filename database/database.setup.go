29cfeb15a4e6195a02296834bd4dd241d0b5dda4
Xenn-00 <node.xenon@gmail.com>
2023-04-08 13:12:46 +0700

    update

--- database/database.setup.go
+++ database/database.setup.go
@@ -1,13 +1,41 @@
 package database
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBSetup() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI(""))
+	uri := fmt.Sprintf("mongodb+srv://%s:%s@cluster0.rt3s2o0.mongodb.net/?retryWrites=true&w=majority", os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"))
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("failed to connect to database")
+		return nil
+	}
+	fmt.Println("Succesfully Connected to Database")
+	return client
 }
+
+var Client *mongo.Client = DBSetup()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection
 
