d8abe826d18fb53984409c193c01a8703e46f617
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-04-08 21:11:56 +0700

    update

--- controllers/address.controller.go
+++ controllers/address.controller.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Xenn-00/go-merce/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func AddAddress() gin.HandlerFunc
 
@@ -8,5 +17,34 @@ func EditAddress() gin.HandlerFunc
 
 func EditWorkAddress() gin.HandlerFunc
 
-func DeleteAddress() gin.HandlerFunc
+func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Id := c.Query("id")
 
+		if Id == "" {
+			c.Header("Content-type", "application/json")
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"status_code": http.StatusBadRequest,
+				"error":       "Invalid search index",
+			})
+			return
+		}
+
+		addresses := make([]models.Address, 0)
+		user_id, err := primitive.ObjectIDFromHex(Id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{
+				"status_code": http.StatusInternalServerError,
+				"error":       err.Error(),
+			})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: user_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+
+	}
+}
