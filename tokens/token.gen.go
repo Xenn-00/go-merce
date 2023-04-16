07a70bbda9e37f838cc7e0e305b6e3b1d1d48794
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-04-16 15:22:13 +0700

    update and validate token

--- tokens/token.gen.go
+++ tokens/token.gen.go
@@ -1,13 +1,17 @@
 package token
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/Xenn-00/go-merce/database"
 	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -50,6 +54,49 @@ func TokenGenerator(email string, firstname string, lastname string, uid string)
 	return token, refreshToken, nil
 }
 
-func ValidateToken()
+func ValidateToken(signedToken string) (claims *SignedDetails, message string) {
+	token, err := jwt.ParseWithClaims(signedToken, &SignedDetails{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SECRET_KEY), nil
+	})
+	if err != nil {
+		message = err.Error()
+		return
+	}
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		message = "invalid token"
+		return
 
-func UpdateAllToken()
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		message = "token is already expired"
+		return
+	}
+	return claims, message
+}
+
+func UpdateAllToken(signedToken string, signedRefreshToken string, userId string) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var updateObj primitive.D
+	updateObj = append(updateObj, bson.E{Key: "token", Value: signedToken})
+	updateObj = append(updateObj, bson.E{Key: "refresh_token", Value: signedRefreshToken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{Key: "updatedAt", Value: updated_at})
+
+	upsert := true
+
+	filter := bson.M{"user_id": userId}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_, err := UserData.UpdateOne(ctx, filter, bson.D{{
+		Key:   "$set",
+		Value: updateObj,
+	}},
+		&opt)
+	defer cancel()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+}
