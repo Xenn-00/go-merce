42567b2a596cb8457612197382d3519be9518d45
Xenn-00 <node.xenon@gmail.com>
2023-04-16 15:44:33 +0700

    auth

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,31 @@
 package middleware
+
+import (
+	"net/http"
+
+	token "github.com/Xenn-00/go-merce/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.AbortWithStatusJSON(http.StatusBadGateway, gin.H{
+				"error": "no authorization header",
+			})
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.AbortWithStatusJSON(http.StatusBadGateway, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
