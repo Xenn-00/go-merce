e89f87896bc609881b303642aa74d157d40ca290
Xenn-00 <node.xenon@gmail.com>
2023-04-16 15:44:23 +0700

    add product to cart

--- database/cart.db.go
+++ database/cart.db.go
@@ -1,6 +1,15 @@
 package database
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/Xenn-00/go-merce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var (
 	ErrCantFindProduct    = errors.New("sorry, we can't find the products")
@@ -12,7 +21,54 @@ var (
 	ErrCantBuyCartItem    = errors.New("can't update the purchase")
 )
 
-func AddProductToCart()
+func AddProductToCart(ctx context.Context, prodCollection *mongo.Collection, userCollection *mongo.Collection, prodID primitive.ObjectID, userId string) error {
+	// check product from db first
+	searchFromDB, err := prodCollection.Find(ctx, bson.M{"_id": prodID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchFromDB.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{
+		primitive.E{
+			Key:   "_id",
+			Value: id,
+		},
+	}
+	update := bson.D{
+		{
+			Key: "$push",
+			Value: bson.D{
+				primitive.E{
+					Key: "user_cart",
+					Value: bson.D{
+						{
+							Key:   "$each",
+							Value: productCart,
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+
+	return err
+}
 
 func RemoveCartItem()
 
