05d55967b8f0662f769df3beaa914550bcf093ed
Xenn-00 <node.xenon@gmail.com>
2023-04-18 18:35:50 +0700

    update main.go

--- main.go
+++ main.go
@@ -25,10 +25,15 @@ func main() {
 	routes.UserRoutes(router)
 	router.Use(middleware.Authentication())
 
-	router.GET("/addtocart", app.AddToCart)
-	router.GET("/removeitem", app.RemoveItem)
-	router.GET("/checkout", app.BuyFromCart)
-	router.GET("/instantbuy", app.InstantBuy)
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", app.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
 
 	log.Fatal(router.Run("localhost:" + port))
 }
